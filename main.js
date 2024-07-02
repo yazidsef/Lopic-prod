6b30ae156c72ddc7082d2585b60d3c5fab98c894
yazid sefsaf <yazidsefsaf45@yahoo.com>
2024-07-02 20:44:17 +0200

    ajout des images de header et faire les transition

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle('window-scrolled'), window.scrollY > 0;
+    document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
+})
+
+//contact button (circular text buttons)
+const textButtons = document.querySelectorAll('.contact_btn');
+
+textButtons.forEach(textButton => {
+    let text = textButton.querySelector('p');
+    text.innerHTML= text.innerHTML.split('').map((character, index) => 
+        `<span style="transform:rotate(${index * 12 }deg)">${character}</span>`).join('');
 })
 
