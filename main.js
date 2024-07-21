aa4402029cd307f8da2349c4d124ed380951813a
yazid sefsaf <yazidsefsaf45@yahoo.com>
2024-07-21 23:03:28 +0200

    ajour de swiperjs

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
 })
-
 //contact button (circular text buttons)
 const textButtons = document.querySelectorAll('.contact_btn');
 
